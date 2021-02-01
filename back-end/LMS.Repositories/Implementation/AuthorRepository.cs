af61901f69e10ada5ac181530e0adaafe1c7179f
aliahsannoon <alinoonciit@gmail.com>
2021-02-01 22:49:18 +0500

    Author Listing

--- back-end/LMS.Repositories/Implementation/AuthorRepository.cs
+++ back-end/LMS.Repositories/Implementation/AuthorRepository.cs
@@ -1,8 +1,11 @@
 ﻿using LMS.Common.Models;
 using LMS.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LMS.Repositories.Implementation
 {
@@ -12,6 +15,11 @@ namespace LMS.Repositories.Implementation
         public AuthorRepository(LMSDBContext context):base(context)
         {
             this.dbContext = context;
+        }
+
+        public async Task<List<Author>> GetAuthorsDetailsAsync()
+        {
+            return await this.dbContext.Authors.Where(del => del.IsDeleted == false).ToListAsync();
         }
     }
 }
--- back-end/LMS.Repositories/Interfaces/IAuthorRepository.cs
+++ back-end/LMS.Repositories/Interfaces/IAuthorRepository.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using LMS.Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LMS.Repositories.Interfaces
 {
     public interface IAuthorRepository
     {
+        public Task<List<Author>> GetAuthorsDetailsAsync();
     }
 }
